63e355afd93f4f22d0d14d0e533bf9ecae71c625
Max Reshetnik <mareshetnik@gmail.com>
2021-05-07 02:15:08 +0300

    add ContentType in shop.models: Category, Product; make and apply migrations for shop

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,8 @@
-from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 
+from django.db import models
 
 # 1 Category
 # 2 Product
@@ -11,24 +13,27 @@ from django.contrib.auth import get_user_model
 # *******************
 # 7 Order
 
+
 class Category(models.Model):
 
-    name = models.CharField(max_length=30)
-    slug = models.SlugField(unique=True)
+    tag = models.SlugField(unique=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
     def __str__(self):
-        return self.name
+        return self.tag
 
 
 class Product(models.Model):
 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=60)
+    tags = GenericRelation(Category, related_query_name='product')
+    name = models.CharField(max_length=60)
     image = models.ImageField()
     description = models.TextField(null=True)
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class ProductSpecification(models.Model):
@@ -37,7 +42,7 @@ class ProductSpecification(models.Model):
                     ('BX', 'Box'), ('LT', 'Lot')]
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    feature_name = models.CharField(max_length=60)
+    feature_name = models.CharField(max_length=40)
     image = models.ImageField()
     characteristic = models.TextField(null=True)
     unit_of_measure = models.CharField(max_length=2, choices=UNIT_CHOICES)
@@ -45,17 +50,21 @@ class ProductSpecification(models.Model):
     price = models.DecimalField(max_digits=9, decimal_places=2)
 
     def __str__(self):
-        return f'{self.product.title}: {self.feature_name}'
+        return f'{self.product}: {self.feature_name}'
 
 
 class Cart(models.Model):
 
-    user = models.OneToOneField(get_user_model())
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     quantity_in_cart = models.PositiveIntegerField(default=0)
     value = models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
 
     def __str__(self):
-        return f"{self.user.username}'s cart"
+        return f"{self.user}'s cart"
 
 
 class CartProduct(ProductSpecification):
@@ -65,10 +74,11 @@ class CartProduct(ProductSpecification):
     total_price = models.DecimalField(max_digits=9, decimal_places=2)
 
     def __str__(self):
-        return f'{self.product.title}: {self.feature_name} in cart'
+        return f'{self.product}: {self.feature_name} in cart'
 
 
 class Customer(models.Model):
+
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=90)
     address = models.TextField()
